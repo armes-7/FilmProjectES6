1e9db3817ce3b030f740eb93ae7a027c9d92cf1d
ZorakiMühendis <46242971+armes-7@users.noreply.github.com>
2021-01-21 10:39:29 +0300

    Add files via upload

---
+++ ui.js
@@ -0,0 +1,76 @@
+class UI{
+
+    static addFilmToUI(newFilm){
+
+        //    <tr>
+        //     <td><img src="" class="img-fluid img-thumbnail"></td>
+        //     <td></td>
+        //     <td></td>
+        //     <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+        //   </tr> 
+        
+        const filmList= document.getElementById("films");
+        filmList.innerHTML +=`
+        <tr>
+          <td><img src="${newFilm.url}" class="img-fluid img-thumbnail"></td>
+             <td>${newFilm.title}</td>
+             <td>${newFilm.director}</td>
+             <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+         </tr> 
+        
+        `;
+        }
+        static clearInput(element1,element2,element3){
+            element1.value ="";
+            element2.value="";
+            element3.value="";
+        }
+        static displayMessages(message,type){
+            const cardBody = document.querySelector(".card-body");
+            //şimdi bunu seçtik bizim alert divini oluşturmamız lazım
+            const div =document.createElement("div");
+            div.className=`alert alert-${type}`;
+            div.textContent=message;
+            //şimdi biz bu alerti oluşturduk bunu card-body child olarak eklememiz gerek
+            cardBody.appendChild(div);
+            
+            //ne kadar süre kalsın 
+        
+            setTimeout(function(){
+                div.remove();
+            },1000);  
+        }
+       static loadAllFilms(films){
+        
+        const filmList = document.getElementById("films");
+        films.forEach(function(film) {
+            filmList.innerHTML +=`<tr>
+            <td><img src="${film.url}" class="img-fluid img-thumbnail"></td>
+               <td>${film.title}</td>
+               <td>${film.director}</td>
+               <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+           </tr> `;
+            
+        });
+        
+         }
+        static deleteFilmFromUI(element){
+            element.parentElement.parentElement.remove();
+        }
+        static clearAllFilmsFromUI(){
+        
+            const filmList = document.getElementById("films");
+            //filmList.innerHTML=""; Böylede yapabiliriz aslında 
+        
+            while(filmList.firstElementChild !==null){//child olduğu sürece
+                filmList.firstElementChild.remove();
+            }
+        }
+            
+        
+        
+
+}
+
+
+
